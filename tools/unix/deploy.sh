e23aa172fe40011f058c001e4fb92b957a711d0f
suke <suke@suke-blog.com>
2018-05-11 11:55:50 +0900

    add test script

---
+++ tools/unix/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ./target/*.mwm /media/kiri/SD-CARD/Android/data/com.mapswithme.maps.pro/files/MapsWithMe/180316/
+
+cp ../../data/drules_proto_*.bin /media/kiri/SD-CARD/Android/data/com.mapswithme.maps.pro/files/MapsWithMe/styles/
+
