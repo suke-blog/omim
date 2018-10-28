76767ee8ba508a2320410f6257228cafbd6dfd8f
suke <suke@suke-blog.com>
2018-10-28 20:21:44 +0900

    add script to make classificator.obb and styles.

---
+++ tools/unix/make_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir config
+pushd config
+
+cp ../../../data/classificator.txt ../../../data/types.txt ../../../data/colors.txt ./
+zip -0j classificator_mod.zip classificator.txt types.txt colors.txt
+mv classificator_mod.zip classificator_mod_$(date -I).obb
+
+mkdir styles
+cp -r ../../../data/resources-* ./styles/                                                      
+cp ../../../data/drules_proto.bin ../../../data/drules_proto_clear.bin ../../../data/drules_proto_dark.bin  ./styles/
+
+popd
+
