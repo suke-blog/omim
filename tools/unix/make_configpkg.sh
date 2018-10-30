107e26f70efed63c3405624627d0f2a1750c5a38
suke <suke@suke-blog.com>
2018-10-30 10:25:44 +0900

    add script to make configure file package.
    リリース用設定ファイルを作成するスクリプトを追加

--- tools/unix/make_configpkg.sh
+++ tools/unix/make_configpkg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir config
-pushd config
+mkdir config_pkg
+pushd config_pkg
 
 cp ../../../data/classificator.txt ../../../data/types.txt ../../../data/colors.txt ./
-zip -0j classificator_mod.zip classificator.txt types.txt colors.txt
+zip -0j classificator_mod.zip classificator.txt types.txt colors.txt 
 mv classificator_mod.zip classificator_mod_$(date -I).obb
 
 mkdir styles
